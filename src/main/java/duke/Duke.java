68a3cf1378bf47591e4abffd49cae3075605a6b3
Bryan Loh <lamofacade@gmail.com>
2021-09-02 03:06:19 +0800

    Refactor static to instance attributes in Duke class

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -14,44 +14,51 @@ import duke.task.TaskCollection;
  * Duke is the class that represents the entire command line application.
  */
 public class Duke {
-
+    private static final String TASK_COLLECTION_STORAGE_PATH = "./data/duke.txt";
     private static final String LOGO = " ____        _        \n"
             + "|  _ \\ _   _| | _____ \n"
             + "| | | | | | | |/ / _ \\\n"
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|\n";
 
-    private static final Ui userInterface = new Ui();
-    private static final String TASK_COLLECTION_STORAGE_PATH = "./data/duke.txt";
-    private static final TaskCollection tasks = new TaskCollection(TASK_COLLECTION_STORAGE_PATH);
-    private static final Queue<Action> actions = new LinkedList<>();
+    private final Ui userInterface = new Ui();
+    private final TaskCollection tasks = new TaskCollection(TASK_COLLECTION_STORAGE_PATH);
+    private final Queue<Action> actions = new LinkedList<>();
+
+    /**
+     * Launches the Duke application.
+     */
+    public void launch() {
+        actions.add(new WelcomeUser());
+
+        while (true) {
+            try {
+                if (actions.isEmpty()) {
+                    String input = userInterface.getUserInput();
+                    Request request = Request.create(tasks, input);
+                    actions.add(request.action());
+                }
+
+                Action action = actions.remove();
+                Response response = action.execute();
+                tasks.saveTasks();
+                userInterface.printResponse(response);
+
+                if (action instanceof GoodbyeUser) {
+                    break;
+                }
+            } catch (UserException exception) {
+                userInterface.printResponse(exception.toResponse());
+            }
+        }
+    }
 
     /**
      * Starts the Duke application.
      * @param args Arguments passed when running the function using java.
      */
     public static void main(String[] args) {
-        Duke.actions.add(new WelcomeUser());
-
-        while (true) {
-            try {
-                if (Duke.actions.isEmpty()) {
-                    String input = Duke.userInterface.getUserInput();
-                    Request request = Request.create(Duke.tasks, input);
-                    Duke.actions.add(request.action());
-                }
-
-                Action action = Duke.actions.remove();
-                Response response = action.execute();
-                Duke.tasks.saveTasks();
-                Duke.userInterface.printResponse(response);
-
-                if (action instanceof GoodbyeUser) {
-                    break;
-                }
-            } catch (UserException exception) {
-                Duke.userInterface.printResponse(exception.toResponse());
-            }
-        }
+        Duke duke = new Duke();
+        duke.launch();
     }
 }
