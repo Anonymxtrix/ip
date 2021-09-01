ef972a85155664126d13e549cfea158119b18455
Bryan Loh <lamofacade@gmail.com>
2021-09-02 03:09:46 +0800

    Create JavaFX skeleton

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -9,11 +9,15 @@ import duke.action.WelcomeUser;
 import duke.exception.UserException;
 import duke.request.Request;
 import duke.task.TaskCollection;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * Duke is the class that represents the entire command line application.
  */
-public class Duke {
+public class Duke extends Application {
     private static final String TASK_COLLECTION_STORAGE_PATH = "./data/duke.txt";
     private static final String LOGO = " ____        _        \n"
             + "|  _ \\ _   _| | _____ \n"
@@ -24,6 +28,15 @@ public class Duke {
     private final Ui userInterface = new Ui();
     private final TaskCollection tasks = new TaskCollection(TASK_COLLECTION_STORAGE_PATH);
     private final Queue<Action> actions = new LinkedList<>();
+
+    /**
+     * Starts the Duke application.
+     * @param args Arguments passed when running the function using java.
+     */
+    public static void main(String[] args) {
+        Duke duke = new Duke();
+        duke.launch();
+    }
 
     /**
      * Launches the Duke application.
@@ -53,12 +66,12 @@ public class Duke {
         }
     }
 
-    /**
-     * Starts the Duke application.
-     * @param args Arguments passed when running the function using java.
-     */
-    public static void main(String[] args) {
-        Duke duke = new Duke();
-        duke.launch();
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }
